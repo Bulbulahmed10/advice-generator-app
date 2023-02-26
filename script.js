2db12c109e74b2f59fcdb597264e7335db6c961e
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-02-26 17:31:30 +0600

    added warning message functionality

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //! global constrain
 const copyImgBtn = document.getElementById("copy-text");
+const warningMessageDiv = document.getElementById("warning-message");
 
 //! fetch data from api
 const loadData = (adviceId) => {
   const URL = `https://api.adviceslip.com/advice${adviceId}`;
+
   try {
     fetch(URL)
       .then((res) => res.json())
@@ -15,20 +17,29 @@ const loadData = (adviceId) => {
   copyImgBtn.style.cursor = "pointer";
 };
 
-
 //! search advice by id
 document.getElementById("search-btn").addEventListener("click", () => {
+  warningMessageDiv.style.visibility = "hidden";
+
   const searchInput = document.getElementById("search-input");
   loadData(`/${searchInput.value}`);
   searchInput.value = "";
 });
 
-
 //! display api data
 const displayData = (advice) => {
-  document.getElementById("advice-id").innerText = advice.id;
-  document.getElementById("advice-body").innerText = `${advice.advice}`;
+  try {
+    document.getElementById("advice-id").innerText = advice.id;
+    document.getElementById("advice-body").innerText = `${advice.advice}`;
+  } catch (err) {
+    warningMessageDiv.style.visibility = "visible";
+  }
 };
+
+//! warning message remove
+document.getElementById("warning-remove").addEventListener("click", () => {
+  warningMessageDiv.style.visibility = "hidden";
+});
 
 //! copy button functionality
 copyImgBtn.addEventListener("click", () => {
